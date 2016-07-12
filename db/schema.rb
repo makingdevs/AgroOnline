fd6566dbe2be528daedb512531ebc0f5a0e5ca25
says <says.rodriguez@gmail.com>
2016-07-12 11:53:12 -0500

    #3 adding the change in the db schema for provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712154619) do
+ActiveRecord::Schema.define(version: 20160712161052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20160712154619) do
     t.string   "min_order_quantity"
     t.string   "origin"
     t.string   "packaging"
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "lastName"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
