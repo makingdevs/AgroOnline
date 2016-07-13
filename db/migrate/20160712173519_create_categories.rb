428696ba43a69404c9de106d7405fc05368e044f
says <says.rodriguez@gmail.com>
2016-07-13 16:48:32 -0500

    #5 change error in migration file

--- db/migrate/20160712173519_create_categories.rb
+++ db/migrate/20160712173519_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.integer, :subcategory
+      t.integer :subcategory
       t.string :name
 
       t.timestamps
