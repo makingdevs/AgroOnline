437ab03297db6dd2f56cf4d08997e743ff4a7a0f
says <says.rodriguez@gmail.com>
2016-07-13 16:49:07 -0500

    #5 add new route of method providers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :products
 
   root 'welcome#index'
+
+  get '/provider/multi/', to: 'providers#multi', as: 'multiProviders'
+  post '/provider/save/', to: 'providers#save', as: 'saveProviders'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
