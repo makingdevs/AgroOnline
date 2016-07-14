8d575d5c4021d2e8fb519619371498ea4b98307b
says <says.rodriguez@gmail.com>
2016-07-14 06:59:26 -0500

    #5 modify behavior of read excel method

--- lib/readXlsManager/read_xls_manager.rb
+++ lib/readXlsManager/read_xls_manager.rb
@@ -4,10 +4,10 @@ class ReadXlsManager
   include Singleton
 
   def readExcel(file)
-    resultRow = []
     resultCell= []
     workbook = RubyXL::Parser.parse(file)
     workbook[0].each_with_index { |row,index|
+      resultRow = []
       if index != 0
         row.cells.each { |cell|
           val = cell && cell.value
