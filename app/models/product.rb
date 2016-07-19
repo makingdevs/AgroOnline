27a1fb7e2bb5e15b1012d8ed8096bd8bd90ef444
says <says.rodriguez@gmail.com>
2016-07-19 07:10:45 -0500

    #10 adding private method of create a product and add relationships into product and 2 different class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+  belongs_to :category
+  belongs_to :provider
+
+  def self.createProduct(params,category)
+    product = Product.new()
+    product.name = params[0]
+    product.sku = params[1]
+    product.price = params[2]
+    product.weight = params[3]
+    product.min_order_quantity = params[4]
+    product.origin = params[5]
+    product.packaging = params[6]
+    product.category = category
+    product.save
+    product
+  end
+
 end
