b93b46ef67b4e4b639db420f297ae8cee2ee6968
says <says.rodriguez@gmail.com>
2016-07-20 17:11:04 -0500

    #12 add requiere for emun class and create method index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
+require 'utilities/estados'
+
 class WelcomeController < ApplicationController
+
+  def index
+    @estados = []
+    Estados.estados.first().each_slice(4) { | estado |
+      @estados << estado
+    }
+  end
+
 end
