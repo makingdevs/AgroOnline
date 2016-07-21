177a9c890fcf80cbcee586852b03f2e91b945636
says <says.rodriguez@gmail.com>
2016-07-21 15:14:04 -0500

    #14 adding route of new methos search provider

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   post '/provider/save/', to: 'providers#save', as: 'saveProviders'
 
   get "/categories/search/:id", to: 'categories#search', as: 'categorySearch'
+  get "/provider/search/:id", to: 'providers#search', as: 'provider_search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
