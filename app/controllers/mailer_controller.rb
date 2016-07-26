92fcd24d7ad1e49c0525f71db83d5e8e97a07fdb
says <says.rodriguez@gmail.com>
2016-07-26 16:05:07 -0500

    #17 adding log to mailer_controller

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -1,6 +1,7 @@
 class MailerController < ApplicationController
 
   def contact
+    puts "send email to provider"
     UserMailer.welcome_email(params).deliver
     @product = Product.find(params[:id])
     redirect_to @product
