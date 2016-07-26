1be0258d82e737b958f60e8170d0e4f33e2667d2
says <says.rodriguez@gmail.com>
2016-07-26 18:18:46 -0500

    #18 adding new route to file route for search states

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/categories/search/:id", to: 'categories#search', as: 'categorySearch'
   get "/product/search/:id", to: 'products#search', as: 'product_search'
   post "/provider/contact/:id", to: 'mailer#contact', as: 'provider_contact'
+  post "/product/category/state", to: 'welcome#search', as: 'state_search_categories'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
