ae7ccd4e0f91be1f05fb9e150cd3b85dcd4f0069
says <says.rodriguez@gmail.com>
2016-07-28 16:08:28 -0500

    #20 adding new variables for email contact

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,8 @@ class UserMailer < ApplicationMailer
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
+    @quantity = params[:cantidad]
+    @unidad = params[:unidad]
     mail(to: 'sergio@makingdevs.com', subject: 'New Message of Nos Unimos')
   end
 
