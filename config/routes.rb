360f5f0161ed8ec2b6b0b90f153bffb6b4eeae40
says33 <says.rodriguez@gmail.com>
2016-08-04 16:59:56 -0500

    #26 adding route of new method of about page

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   post "/product/category/state", to: 'welcome#search', as: 'state_search_categories'
   get "/product/contact", to: 'products#contact', as: 'product_provider_contact'
 
+  get "/welcome/our", to: 'welcome#our', as: 'welcome_out'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
