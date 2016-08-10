2ef74fbf796236bf2c59992e596908357dce08b8
says33 <says.rodriguez@gmail.com>
2016-08-10 10:41:10 -0500

    #29 adding address to provider

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -8,12 +8,13 @@ class Provider < ApplicationRecord
 
   validates :nickname, presence: true
 
-  def self.createProvider(params)
+  def self.createProvider(params,address)
     provider = Provider.new()
     provider.name = params[0]
     provider.lastName = params[1]
     provider.nickname = params[2]
     provider.country = "Mexico"
+    provider.addres = address
     provider.save
     provider
   end
