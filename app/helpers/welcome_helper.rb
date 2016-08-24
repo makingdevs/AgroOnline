ec82a3e49142d9ff7a2a1c81797d124ca6a5f034
says33 <says.rodriguez@gmail.com>
2016-08-24 10:21:16 -0500

    #34 adding created user_in_session helper for show login button or name of user loged and logout button

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,16 @@
 module WelcomeHelper
+  def user_in_session
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      content_tag(:i) do
+        concat " <i class='fa fa-user fa-lg' aria-hidden='true'></i>".html_safe
+        concat " Bienvenido #{@user.username}  "
+        concat link_to("Logout", logout_path, :class => "waves-effect waves-light btn")
+      end
+    else
+      content_tag(:i) do
+        concat link_to("LogIn", login_path, :class => "waves-effect waves-light btn")
+      end
+    end
+  end
 end
