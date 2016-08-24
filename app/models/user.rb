3b788909bda2fd8a21918c0e1284903ff1123a88
says33 <says.rodriguez@gmail.com>
2016-08-24 10:25:02 -0500

    #34 adding belogns to for provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   has_secure_password
+
+  belongs_to :provider
+
 end
