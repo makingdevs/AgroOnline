291f4f11b63e43d7ed9a3d3c0cf65e2fc60c459a
says33 <says.rodriguez@gmail.com>
2016-08-26 13:08:48 -0500

    #35 remove elements of product model class

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,18 +8,4 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :provider
 
-  def self.createProduct(params,category)
-    product = Product.new()
-    product.name = params[0]
-    product.sku = params[1]
-    product.price = params[2]
-    product.weight = params[3]
-    product.min_order_quantity = params[4]
-    product.origin = params[5]
-    product.packaging = params[6]
-    product.category = category
-    product.save
-    product
-  end
-
 end
