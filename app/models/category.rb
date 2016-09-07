05bb73d458b8cd9d06bc80eb88df5116bd2f3411
says33 <says.rodriguez@gmail.com>
2016-09-07 08:35:25 -0500

    #43 adding reference to category to s3 asset

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,8 @@ class Category < ActiveRecord::Base
    include Elasticsearch::Model
    include Elasticsearch::Model::Callbacks
 
+   belongs_to :s3_asset, optional:true
+
   def to_s
      name
    end
