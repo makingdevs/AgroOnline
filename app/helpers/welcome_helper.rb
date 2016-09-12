066125504afbe2d00e732418b49342e4c34a0a46
says33 <says.rodriguez@gmail.com>
2016-09-12 15:59:57 -0500

    #48 delete helper for login and singin

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,21 +1,2 @@
 module WelcomeHelper
-  def user_in_session
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      content_tag(:i) do
-        concat " <i class='fa fa-user fa-lg' aria-hidden='true'></i>".html_safe
-        concat " Bienvenido #{@user.username}  "
-        if @user.provider
-          concat link_to(" <small>Cuenta </small> &ensp;".html_safe, profile_path(@user.try(:provider).try(:id)))
-        end
-        concat link_to("Salir", logout_path, :class => "waves-effect waves-light btn")
-      end
-    else
-      content_tag(:i) do
-        concat link_to("Crear Cuenta", new_user_path, :class => "waves-effect waves-light btn")
-        concat "  "
-        concat link_to("Acceder", login_path, :class => "waves-effect waves-light btn")
-      end
-    end
-  end
 end
