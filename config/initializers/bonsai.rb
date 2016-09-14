f347a8a0843fba5fe052e61ca88a2ac42dbe2712
says33 <says.rodriguez@gmail.com>
2016-09-14 08:39:42 -0500

    #51 change use of obtains variable for environment prod for bonsai elasticsearch

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,7 @@
 require 'elasticsearch/model'
 
-if ENV['BONSAI_URL']
-    Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+bonsai = Rails.application.secrets.bonsai_url
+
+if bonsai
+    Elasticsearch::Model.client = Elasticsearch::Client.new({url: bonsai, logs: true})
 end
