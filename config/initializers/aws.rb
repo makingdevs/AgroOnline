541079f2f9f15c2b750221653f11f8251c644ef0
says33 <says.rodriguez@gmail.com>
2016-09-14 08:38:29 -0500

    #51  change use of extrenal variable for obtains of secrets yml

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,2 @@
 require 'aws-sdk'
-Aws.config[:credentials] = Aws::Credentials.new(ENV['aws_access_key_id'] , ENV['aws_secret_access_key'])
+Aws.config[:credentials] = Aws::Credentials.new(Rails.application.secrets.aws_access_key_id ,Rails.application.secrets.aws_access_key)
