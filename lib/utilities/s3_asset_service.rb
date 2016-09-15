8014d54f538e098fffd3234c8e8bd0c15c73a796
says33 <says.rodriguez@gmail.com>
2016-09-15 10:03:08 -0500

    #60 change the obtains environments variables of s3

--- lib/utilities/s3_asset_service.rb
+++ lib/utilities/s3_asset_service.rb
@@ -4,16 +4,16 @@ class S3AssetService
   include Singleton
 
   def initialize
-    @bucket_region = ENV['aws_bucket_region']
-    @bucket_name = ENV['aws_bucket_name']
+    @bucket_region = Rails.application.secrets.aws_bucket_region
+    @bucket_name = Rails.application.secrets.aws_bucket_name
   end
 
   def upload_image_to_s3(params)
-     Aws.use_bundled_cert!
-     s3 = Aws::S3::Resource.new(region: @bucket_region)
-     file_to_upload = s3.bucket(@bucket_name).object("#{Time.now()}_#{params['file'].original_filename}")
-     file_to_upload.upload_file(params['file'].tempfile, acl:'public-read')
-     file_to_upload
+    Aws.use_bundled_cert!
+    s3 = Aws::S3::Resource.new(region: @bucket_region)
+    file_to_upload = s3.bucket(@bucket_name).object("#{Time.now()}_#{params['file'].original_filename}")
+    file_to_upload.upload_file(params['file'].tempfile, acl:'public-read')
+    file_to_upload
   end
 
   def save_image_s3_asset(url_photo_s3,name_photo)
