679219d3f6ef282da50b459d6c82f1cc650ba920
says33 <says.rodriguez@gmail.com>
2016-10-19 15:20:51 -0500

    mexicandebuggin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'nosunimos.herokuapp.com' }
 
-  puts Rails.application.secrets.inspect
+  puts Rails.application.secrets.mailer_host
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
