4c2a22ec8760afb271f046c59d989802355d7e93
says33 <says.rodriguez@gmail.com>
2016-10-19 15:07:24 -0500

    mexicandebuggin

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,3 @@
 require 'aws-sdk'
+puts Rails.application.mailer_host
 Aws.config[:credentials] = Aws::Credentials.new(Rails.application.secrets.aws_access_key_id ,Rails.application.secrets.aws_access_key)
