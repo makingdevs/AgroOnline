ef5b058b5de4bbbb591b4e71714019ff384e0e63
says33 <says.rodriguez@gmail.com>
2016-10-20 10:25:45 -0500

    #63 change logic in used of stylesheets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile = ['*.js', '*.css', '**/*.js', '**/*.css']
+
   config.assets.digest = true
 
   config.cache_classes = true
